bc8e8a17177c729e4aaa1a7e4c06dc1e5fe9f3cc
Rebecca Shillander <becca.shilly@gmail.com>
2020-11-17 11:02:52 -0800

    more setup instructions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Step 1: install my SSH key
+# Step 2: download my dotfiles github repo
+# Step 3: in command line, run   $HOME/Projects/dotfiles/setup.sh
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 ln -s /Users/rms/Documents/Projects/dotfiles/.zshrc /Users/rms/.zshrc
+
+mkdir -p $HOME/Documents/Projects
+git clone git@github.com:rebeccamarie/FamilyTrunkWebsite.git $HOME/Documents/Projects
